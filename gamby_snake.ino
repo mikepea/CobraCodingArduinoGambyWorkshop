329add647b1590e10422b32e9b86d816677153ba
Mike Pountney <Mike.Pountney@gmail.com>
2014-10-26 15:55:43 +0000

    Draw a room for our snake to move in.
    Max out the available block space :)

--- gamby_snake.ino
+++ gamby_snake.ino
@@ -10,7 +10,7 @@ boolean playing;    // 'true' when the game is playing, 'false' after game over
 // block mode -- saves memory
 GambyBlockMode gamby;
 
-const byte ROOML_WIDTH = 20;
+const byte ROOM_WIDTH = 24;
 const byte ROOM_HEIGHT = 15;
 
 const byte INPUT_DELAY = 50;
@@ -20,6 +20,10 @@ PROGMEM prog_uint16_t palette[] = {
   0x0000, //  0: Empty
   0xffff, //  1: Full
 };
+
+// friendly names for common palette blocks:
+const byte EMPTY_BLOCK = 0;
+const byte WALL_BLOCK = 1;
 
 unsigned long lastInputTime; // The time at which gamby.readInputs() was last called
 byte lastInputs;    // The state of the inputs the last time they were checked.
@@ -51,7 +55,23 @@ void loop() {
 }
 
 void startGame() {
+  byte i;
   gamby.clearScreen();
+
+  // The room sides
+  for (i=0; i<ROOM_HEIGHT; i++) {
+    gamby.setBlock(0,i,WALL_BLOCK);
+    gamby.setBlock(ROOM_WIDTH-1, i, WALL_BLOCK);
+  }
+  // The room top and bottom
+  for (i=0; i<ROOM_WIDTH; i++) {
+    gamby.setBlock(i, 0, WALL_BLOCK);
+    gamby.setBlock(i, ROOM_HEIGHT-1, WALL_BLOCK);
+  }
+
+  // setBlock doesn't actually draw. .update does.
+  gamby.update(0,0,ROOM_WIDTH-1,ROOM_HEIGHT-1);
+
   playing = true;
 }
 
