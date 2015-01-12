09b3b4cd0e112452373e81d37a1fdd722530e81f
Mike Pountney <Mike.Pountney@gmail.com>
2015-01-12 08:42:04 +0000

    Add an info panel for score, hiscore
    This means reducing our 'room' accordingly, but things are *much*
    easier if the room size is <256 locations, so that we can use
    a byte array for snake and object positioning.

--- gamby_snake.ino
+++ gamby_snake.ino
@@ -7,11 +7,26 @@
 
 boolean playing;    // 'true' when the game is playing, 'false' after game over
 
-// block mode -- saves memory
+// block mode -- saves memory, and snake is pretty blocky!
 GambyBlockMode gamby;
 
-const byte ROOM_WIDTH = 24;
-const byte ROOM_HEIGHT = 15;
+const byte ROOM_WIDTH = 18;
+const byte ROOM_HEIGHT = 16;
+
+const byte SCORELABEL_X = ROOM_WIDTH*4+2;
+const byte SCORELABEL_Y = 6;
+
+const byte SCOREPOS_X = SCORELABEL_X;
+const byte SCOREPOS_Y = SCORELABEL_Y + 1;
+
+const byte HISCORELABEL_X = SCORELABEL_X;
+const byte HISCORELABEL_Y = 0;
+
+const byte HISCOREPOS_X = SCORELABEL_X;
+const byte HISCOREPOS_Y = HISCORELABEL_Y + 1;
+
+const byte START_X = 8;
+const byte START_Y = 8;
 
 const byte INPUT_DELAY = 50;
 
@@ -30,6 +45,9 @@ byte lastInputs;    // The state of the inputs the last time they were checked.
 
 // Bring in the font from the other tab (font.ino)
 extern prog_int32_t font[];
+
+int score = 0;
+int hiscore = 50;
 
 void setup() {
   gamby.palette = palette;
@@ -72,7 +90,25 @@ void startGame() {
   // setBlock doesn't actually draw. .update does.
   gamby.update(0,0,ROOM_WIDTH-1,ROOM_HEIGHT-1);
 
+  // Print the info display
+  setupInfoDisplay();
+
   playing = true;
+}
+
+void updateInfoDisplay() {
+  gamby.setPos(HISCOREPOS_X,HISCOREPOS_Y);
+  gamby.print(hiscore);
+  gamby.setPos(SCOREPOS_X,SCOREPOS_Y);
+  gamby.print(score);
+}
+
+void setupInfoDisplay() {
+  gamby.setPos(HISCORELABEL_X,HISCORELABEL_Y);
+  gamby.print("Hi");
+  gamby.setPos(SCORELABEL_X,SCORELABEL_Y);
+  gamby.print("Score");
+  updateInfoDisplay();
 }
 
 // Display the splash screen and wait for the user to press a button.
